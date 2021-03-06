0f97fe436979ff9060e815b83f99df5e551bf641
Luan Maia <luangmaiadias@gmail.com>
2021-03-06 17:37:36 -0300

    attributes field group

---
+++ assets/classes/attributes.ts
@@ -0,0 +1,13 @@
+export class Attributes {
+    charisma: Number;
+    strength: Number;
+    wit: Number;
+
+    constructor(charisma: Number,
+        strength: Number,
+        wit: Number) {
+        this.charisma = charisma;
+        this.strength = strength;
+        this.wit = wit;
+    }
+}
---
+++ components/AttributesFieldGroup.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <b-card bg-variant="light">
+      <b-form-group
+        label-cols="3"
+        label="Atributos"
+        label-size="lg"
+        label-class="font-weight-bold pt-0"
+        class="mb-0"
+      >
+        <RatingField
+          title="Cabulosidade"
+          id="strength-attribute-field"
+          v-bind:value="attributes.strength"
+        />
+        <RatingField
+          title="Sagacidade"
+          id="wit-attribute-field"
+          v-bind:value="attributes.wit"
+        />
+        <RatingField
+          title="Malemolência"
+          id="charisma-attribute-field"
+          v-bind:value="attributes.charisma"
+        />
+      </b-form-group>
+    </b-card>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Attributes } from '@/assets/classes/attributes'
+
+export default Vue.extend({
+  props: {
+    attributes: Attributes,
+  },
+})
+</script>
+
+<style>
+</style>
