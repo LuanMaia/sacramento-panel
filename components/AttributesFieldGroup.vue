0674f9bb11716a83c987042f52902bc859ec575f
Luan Maia <luangmaiadias@gmail.com>
2021-03-06 20:32:34 -0300

    cards on dark mode

--- components/AttributesFieldGroup.vue
+++ components/AttributesFieldGroup.vue
@@ -1,29 +1,21 @@
 <template>
   <div>
-    <b-card bg-variant="light">
-      <b-form-group
-        label-cols="3"
-        label="Atributos"
-        label-size="lg"
-        label-class="font-weight-bold pt-0"
-        class="mb-0"
-      >
-        <RatingField
-          title="Cabulosidade"
-          id="strength-attribute-field"
-          v-bind:value="attributes.strength"
-        />
-        <RatingField
-          title="Sagacidade"
-          id="wit-attribute-field"
-          v-bind:value="attributes.wit"
-        />
-        <RatingField
-          title="Malemolência"
-          id="charisma-attribute-field"
-          v-bind:value="attributes.charisma"
-        />
-      </b-form-group>
+    <b-card bg-variant="dark" text-variant="white" title="Atributos">
+      <RatingField
+        title="Cabulosidade"
+        id="strength-attribute-field"
+        v-bind:value="attributes.strength"
+      />
+      <RatingField
+        title="Sagacidade"
+        id="wit-attribute-field"
+        v-bind:value="attributes.wit"
+      />
+      <RatingField
+        title="Malemolência"
+        id="charisma-attribute-field"
+        v-bind:value="attributes.charisma"
+      />
     </b-card>
   </div>
 </template>
--- components/EndurancesFieldGroup.vue
+++ components/EndurancesFieldGroup.vue
@@ -1,28 +1,23 @@
 <template>
   <div>
-    <b-card bg-variant="light">
-      <b-form-group
-        label-cols="3"
-        label="Resistências"
-        label-size="lg"
-        label-class="font-weight-bold pt-0"
-        class="mb-0"
-      >
-        <EnduranceDiceBonusField title="Física" :diceBonus="endurances.physical" />
-        <EnduranceDiceBonusField title="Mental" :diceBonus="endurances.mental" />
-        <EnduranceDiceBonusField title="Social" :diceBonus="endurances.social" />
-      </b-form-group>
+    <b-card bg-variant="dark" text-variant="white" title="Resistências">
+      <EnduranceDiceBonusField
+        title="Física"
+        :diceBonus="endurances.physical"
+      />
+      <EnduranceDiceBonusField title="Mental" :diceBonus="endurances.mental" />
+      <EnduranceDiceBonusField title="Social" :diceBonus="endurances.social" />
     </b-card>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import { Endurances } from '@/assets/classes/endurances';
+import Vue from 'vue'
+import { Endurances } from '@/assets/classes/endurances'
 
 export default Vue.extend({
-    props: {
-        endurances: Endurances
-    }
-});
+  props: {
+    endurances: Endurances,
+  },
+})
 </script>
