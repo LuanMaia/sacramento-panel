a94769aee865778f551cfb3ac8a9371556d28d8c
Luan Maia <luangmaiadias@gmail.com>
2021-03-06 18:08:38 -0300

    fixing attributes class

--- assets/classes/attributes.ts
+++ assets/classes/attributes.ts
@@ -1,13 +1,5 @@
 export class Attributes {
-    charisma: Number;
-    strength: Number;
-    wit: Number;
-
-    constructor(charisma: Number,
-        strength: Number,
-        wit: Number) {
-        this.charisma = charisma;
-        this.strength = strength;
-        this.wit = wit;
-    }
+    charisma?: Number;
+    strength?: Number;
+    wit?: Number;
 }
