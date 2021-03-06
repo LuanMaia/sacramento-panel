276a166997b501e113ad80ee06e80c9bb1dc792c
Luan Maia <luangmaiadias@gmail.com>
2021-03-06 20:13:23 -0300

    endurance attributes bonuses field group

---
+++ assets/classes/dice-type.ts
@@ -0,0 +1,9 @@
+export enum DiceType {
+    DICE_1D4 = '1d4',
+    DICE_1D6 = '1d6',
+    DICE_1D8 = '1d8',
+    DICE_1D10 = '1d10',
+    DICE_1D12 = '1d12',
+    DICE_1D20 = '1d20',
+    DICE_1D100 = '1d100',
+}
---
+++ components/EnduranceDiceBonusField.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <b-form-group
+      label-cols="3"
+      content-cols="4"
+      :label="title"
+      label-align-sm="right"
+      label-size="lg"
+      :description="`${diceBonus.type} + ${diceBonus.dice}`"
+    >
+      <b-form-group
+        label-cols="4"
+        content-cols="8"
+        label="Atributo"
+        label-align-sm="right"
+        label-size="sm"
+      >
+        <b-form-select
+          v-model="diceBonus.type"
+          :options="typeOptions"
+        ></b-form-select>
+      </b-form-group>
+      <b-form-group
+        label-cols="4"
+        content-cols="8"
+        label="Dado"
+        label-align-sm="right"
+        label-size="sm"
+      >
+        <b-form-select
+          v-model="diceBonus.dice"
+          :options="diceOptions"
+        ></b-form-select>
+      </b-form-group>
+    </b-form-group>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { EnduranceDiceBonus } from '@/assets/classes/endurance-dice-bonus'
+import { EnduranceAttributeType } from '@/assets/classes/endurance-attribute-type'
+import { DiceType } from '@/assets/classes/dice-type'
+
+export default Vue.extend({
+  data() {
+    return {
+      typeOptions: Object.values(EnduranceAttributeType),
+      diceOptions: Object.values(DiceType),
+    }
+  },
+  props: {
+    title: String,
+    diceBonus: EnduranceDiceBonus,
+  },
+})
+</script>
