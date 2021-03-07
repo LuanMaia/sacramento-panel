86f7f4c641eb4419dbdf7bfe66993e2b403cf790
Luan Maia <luangmaiadias@gmail.com>
2021-03-06 22:31:57 -0300

    characteristics group

---
+++ components/CharacteristicsGroup.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <b-card
+      bg-variant="dark"
+      text-variant="white"
+      title="Vantagens e desvantagens"
+    >
+      <p v-for="(characteristic, index) in characteristics" :key="index">
+        <span>{{ characteristic.name }}: </span>
+        <span>{{ characteristic.description }}</span>
+      </p>
+    </b-card>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    characteristics: Array,
+  },
+})
+</script>
