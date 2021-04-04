500466b448b9b588d37a4961c11fe4f3fd5854c3
Luan Maia <luangmaiadias@gmail.com>
2021-04-04 19:28:18 -0300

    reducing connections on profile

--- pages/character/profile/index.vue
+++ pages/character/profile/index.vue
@@ -1,19 +1,20 @@
 <template>
   <CharacterProfile
-    :profilePictureUrl="profilePictureUrl"
     :textColor="textColor"
     :glitchType="glitchType"
-    :characterName="characterName"
-    :characterLife="characterLife"
-    :characterMaxLife="characterMaxLife"
-    :playerTag="playerTag"
-    :showLife="showLife"
+    :profilePictureUrl="character.profileAvatarUrl"
+    :characterName="character.name"
+    :characterLife="character.life"
+    :characterMaxLife="character.maxLife"
+    :playerTag="character.playerTag"
+    :showLife="character.showLifeOnProfile"
     :readonly="true"
   />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { Character } from '@/assets/classes/character'
 
 const UUID_QUERY_PARAM_NAME = 'character-uuid'
 const TEXT_COLOR_QUERY_PARAM_NAME = 'text-color'
@@ -22,25 +23,15 @@ const GLITCH_TYPE = 'glitch-type'
 export default Vue.extend({
   data() {
     return {
-      profilePictureUrl: '',
       textColor: 'white',
       glitchType: 3,
-      characterName: '',
-      playerTag: '',
-      characterLife: 0,
-      characterMaxLife: 0,
-      showLife: false,
+      character: new Character(),
     }
   },
   created() {
     const uuidQueryParam = this.$route.query[UUID_QUERY_PARAM_NAME]
     if (uuidQueryParam && typeof uuidQueryParam === 'string') {
-      this.listenProfilePictureURLChange(uuidQueryParam)
-      this.listenToCharacterNameChange(uuidQueryParam)
-      this.listenToPlayerTagChange(uuidQueryParam)
-      this.listenToCharacterLifeChange(uuidQueryParam)
-      this.listenToCharacterMaxLifeChange(uuidQueryParam)
-      this.listenToCharacterShowLifeChange(uuidQueryParam)
+      this.listenToCharacterDataChange(uuidQueryParam)
     }
 
     const textColor = this.$route.query[TEXT_COLOR_QUERY_PARAM_NAME]
@@ -53,70 +44,14 @@ export default Vue.extend({
     }
   },
   methods: {
-    listenProfilePictureURLChange(characterUUID: string): void {
-      const profileAvatarUrlRef = this.$fire.database
+    listenToCharacterDataChange(characterUUID: string): void {
+      const characterRef = this.$fire.database
         .ref('character')
         .child(`${characterUUID}`)
-        .child('profileAvatarUrl')
 
-      profileAvatarUrlRef.on('value', (snapshot) => {
-        const profileAvatarUrl: string = snapshot.val()
-        this.profilePictureUrl = profileAvatarUrl
-      })
-    },
-    listenToCharacterNameChange(characterUUID: string): void {
-      const characterNameRef = this.$fire.database
-        .ref('character')
-        .child(`${characterUUID}`)
-        .child('name')
-
-      characterNameRef.on('value', (snapshot) => {
-        const characterName: string = snapshot.val()
-        this.characterName = characterName
-      })
-    },
-    listenToPlayerTagChange(characterUUID: string): void {
-      const characterPlayerTagRef = this.$fire.database
-        .ref('character')
-        .child(`${characterUUID}`)
-        .child('playerTag')
-
-      characterPlayerTagRef.on('value', (snapshot) => {
-        const playerTag: string = snapshot.val()
-        this.playerTag = playerTag
-      })
-    },
-    listenToCharacterLifeChange(characterUUID: string): void {
-      const characterLifeRef = this.$fire.database
-        .ref('character')
-        .child(`${characterUUID}`)
-        .child('life')
-
-      characterLifeRef.on('value', (snapshot) => {
-        const characterLife: number = snapshot.val()
-        this.characterLife = characterLife
-      })
-    },
-    listenToCharacterMaxLifeChange(characterUUID: string): void {
-      const characterMaxLifeRef = this.$fire.database
-        .ref('character')
-        .child(`${characterUUID}`)
-        .child('maxLife')
-
-      characterMaxLifeRef.on('value', (snapshot) => {
-        const characterMaxLife: number = snapshot.val()
-        this.characterMaxLife = characterMaxLife
-      })
-    },
-    listenToCharacterShowLifeChange(characterUUID: string): void {
-      const characterShowLifeRef = this.$fire.database
-        .ref('character')
-        .child(`${characterUUID}`)
-        .child('showLifeOnProfile')
-
-      characterShowLifeRef.on('value', (snapshot) => {
-        const showLife: boolean = snapshot.val()
-        this.showLife = showLife
+      characterRef.on('value', (snapshot) => {
+        const characterData = snapshot.val()
+        this.character = this.$convertFirebaseCharacterData(characterData)
       })
     },
   },
